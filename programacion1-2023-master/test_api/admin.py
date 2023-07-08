8ea8473f8e3934843282d9d2eaadeb11679e7daf
AgostinaOrellano <69177598+AgostinaOrellano@users.noreply.github.com>
2023-07-08 18:59:39 -0300

    Delete programacion1-2023-master directory

--- programacion1-2023-master/test_api/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Alumno
-from .models import Reserva
-from .models import Laboratorio
-
-# Register your models here.
-admin.site.register(Alumno)
-admin.site.register(Reserva)
-admin.site.register(Laboratorio)
